db62254d7c3e6b81157ac87774074ea262e6e0eb
Aimen EL ASSIMI <aelassim@IFREMER.FR>
2022-10-28 11:36:39 +0200

    exercices POO

---
+++ Exercices-objet/heritage.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class vehicule
+{
+private:
+    char marque;
+    int year;
+    double prix_ini;
+    double val_act;
+public:
+    vehicule(/* args */);
+    ~vehicchar marque
+    int 
+vehchar marque
+int icule(/* argchar marque
+int 
+vehicule::~vehicchar marque
+int 
